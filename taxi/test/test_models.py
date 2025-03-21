513bd51999ceef6fd09f93a5d56fb413ac61440e
RuslanTopal <baranotik15@gmail.com>
2025-03-21 03:16:02 +0000

    add test for admin page

--- taxi/test/test_models.py
+++ taxi/test/test_models.py
@@ -1,3 +1,12 @@
+import os
+import django
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi_service.settings")
+django.setup()
+
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from taxi.models import Manufacturer, Driver, Car
